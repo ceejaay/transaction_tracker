de0c7c30c9049f4d29086de8dd3a8affebdaa523
Chad Jemmett <cjemmett@wehco.com>
2021-11-24 17:17:40 -0600

    Can connect to and insert into postgres

--- flask_api/seed.py
+++ flask_api/seed.py
@@ -1,5 +1,27 @@
-from sqlalchemy import create_engine
-DATABASE_URI = 'postgresql+psycopg2://cjem:password@hostname/homework_dev'
-engine = create_engine(DATABASE_URI)
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import declarative_base, sessionmaker
+DATABASE_URI = 'postgresql+psycopg2://cjem:password@localhost/friends_repo'
+Base = declarative_base()
 
+engine = create_engine(DATABASE_URI, echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+
+user = User(name="Chad")
+session.add(user)
+session.commit()
+# Base.metadata.create_all(engine)
+
+
+# User.__table__
+# Table('users', MetaData(),
+#         Column('id', Integer(), table='friends_repo', primary_key=True, nullable=False),
+#         Column('name', String(), table='friends_repo')
+#         )
 
