ab55f8ce615cb51773797cf6484b9bca8cfb8c1e
Chad Jemmett <cjemmett@wehco.com>
2021-11-24 17:39:46 -0600

    Can write to postgres. Working on writing to homework_dev

--- flask_api/seed.py
+++ flask_api/seed.py
@@ -1,6 +1,12 @@
-from sqlalchemy import create_engine, Column, Integer, String
+import uuid
+from datetime import datetime
+from sqlalchemy import create_engine, Column, Integer, String, DateTime
 from sqlalchemy.orm import declarative_base, sessionmaker
-DATABASE_URI = 'postgresql+psycopg2://cjem:password@localhost/friends_repo'
+from sqlalchemy.dialects.postgresql import UUID
+from faker import Faker
+
+fake = Faker()
+DATABASE_URI = 'postgresql+psycopg2://cjem:password@localhost/homework_dev'
 Base = declarative_base()
 
 engine = create_engine(DATABASE_URI, echo=True)
@@ -9,14 +15,26 @@ session = Session()
 
 class User(Base):
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
+    id = Column(UUID(as_uuid=True), primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    dob = Column(String)
+    inserted_at=Column(DateTime, nullable=False)
+    updated_at=Column(DateTime, nullable=False)
 
 
-user = User(name="Chad")
+user = User(id=uuid.uuid1(), first_name="Chad", last_name="Jem", dob="02251875", inserted_at=datetime.now(),
+        updated_at=datetime.now)
+
 session.add(user)
 session.commit()
-# Base.metadata.create_all(engine)
+
+# for i in range(1, 10):
+#     print(i)
+#     user = User(name=fake.name())
+#     session.add(user)
+#     session.commit()
+    # Base.metadata.create_all(engine)
 
 
 # User.__table__
